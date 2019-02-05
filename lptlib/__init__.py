5d4934d1f32820c5d641eb0b3fbfbd48d92617e9
Richard Neumann <r.neumann@homeinfo.de>
2019-02-05 12:10:06 +0100

    Added model classes to __init__.

--- lptlib/__init__.py
+++ lptlib/__init__.py
@@ -1,6 +1,7 @@
 """Local public transport API."""
 
 from lptlib.api import get_departures
+from lptlib.common import Stop, StopEvent
 
 
-__all__ = ['get_departures']
+__all__ = ['get_departures', 'Stop', 'StopEvent']
