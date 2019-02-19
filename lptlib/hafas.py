677d2df34fc36fb4d7b45f45839b0f38a27b9ac7
Richard Neumann <r.neumann@homeinfo.de>
2019-02-19 15:54:40 +0100

    Refac.

--- lptlib/hafas.py
+++ lptlib/hafas.py
@@ -7,10 +7,20 @@ from lptlib.config import MAX_STOPS, MAX_DEPARTURES
 __all__ = ['get_departures']
 
 
+def _stop_events(departures):
+    """Yields stop events of a Departure node."""
+
+    for depc, departure in enumerate(departures, start=1):
+        if depc > MAX_DEPARTURES:
+            break
+
+        yield StopEvent.from_hafas(departure)
+
+
 def get_departures(client, address):
     """Returns departures from the respective HAFAS client."""
 
-    location_list = client.locations(repr(address), type='S')  # Stations only.
+    location_list = client.locations(str(address), type='S')  # Stations only.
     stop_locations = location_list.StopLocation
     stops = []
 
@@ -19,15 +29,7 @@ def get_departures(client, address):
             break
 
         departure_board = client.departure_board(stop_location.id)
-        departures = []
-
-        for depc, departure in enumerate(departure_board.Departure, start=1):
-            if depc > MAX_DEPARTURES:
-                break
-
-            departure = StopEvent.from_hafas(departure)
-            departures.append(departure)
-
+        departures = list(_stop_events(departure_board.Departure))
         stop = Stop.from_hafas(stop_location, departures)
         stops.append(stop)
 
--- lptlib/trias.py
+++ lptlib/trias.py
@@ -7,10 +7,20 @@ from lptlib.config import MAX_STOPS, MAX_DEPARTURES
 __all__ = ['get_departures']
 
 
+def _stop_events(stop_event_results):
+    """Yields stop events."""
+
+    for depc, stop_event_result in enumerate(stop_event_results, start=1):
+        if depc > MAX_DEPARTURES:
+            break
+
+        yield StopEvent.from_trias(stop_event_result)
+
+
 def get_departures(client, address):
     """Returns departures from the respective Trias client."""
 
-    geo_coordinates = client.geocoordinates(repr(address))
+    geo_coordinates = client.geocoordinates(str(address))
     trias = client.stops(geo_coordinates)
     payload = trias.ServiceDelivery.DeliveryPayload
     locations = payload.LocationInformationResponse.Location
@@ -24,15 +34,7 @@ def get_departures(client, address):
         trias = client.stop_event(stop_point_ref)
         payload = trias.ServiceDelivery.DeliveryPayload
         stop_event_results = payload.StopEventResponse.StopEventResult
-        departures = []
-
-        for depc, stop_event_result in enumerate(stop_event_results, start=1):
-            if depc > MAX_DEPARTURES:
-                break
-
-            departure = StopEvent.from_trias(stop_event_result)
-            departures.append(departure)
-
+        departures = list(_stop_events(stop_event_results))
         stop = Stop.from_trias(location, departures)
         stops.append(stop)
 
