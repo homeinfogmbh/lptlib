91dc55664b4f3591fbc6912fc0cae6e3af09a1f0
Richard Neumann <mail@richard-neumann.de>
2021-09-30 11:50:26 +0200

    Export GeoCoordinates

--- lptlib/__init__.py
+++ lptlib/__init__.py
@@ -1,6 +1,7 @@
 """Local public transport API."""
 
 from lptlib.api import get_departures, get_response
+from lptlib.datastructures import GeoCoordinates
 
 
-__all__ = ['get_departures', 'get_response']
+__all__ = ['get_departures', 'get_response', 'GeoCoordinates']
