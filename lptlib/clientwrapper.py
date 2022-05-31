0b2993a0175a765406075aabe68b3c249207556d
Richard Neumann <r.neumann@homeinfo.de>
2022-05-31 10:35:37 +0200

    Update indentation

--- lptlib/clientwrapper.py
+++ lptlib/clientwrapper.py
@@ -19,8 +19,12 @@ Client = Union[HafasClient, TriasClient]
 class ClientWrapper:   # pylint: disable=R0903
     """A generic local public transport API client."""
 
-    def __init__(self, client: Client, source: str,
-                 fix_address: bool = False):
+    def __init__(
+            self,
+            client: Client,
+            source: str,
+            fix_address: bool = False
+    ):
         """Sets client and source."""
         self.client = client
         self.source = source
@@ -29,16 +33,22 @@ class ClientWrapper:   # pylint: disable=R0903
     def __str__(self):
         return f'{self.client} {self.source} {self.fix_address}'
 
-    def get_departures_geo(self, geo: GeoCoordinates, *,
-                           stops: Optional[int] = None,
-                           departures: Optional[int] = None
-                           ) -> Iterator[Stop]:
+    def get_departures_geo(
+            self,
+            geo: GeoCoordinates,
+            *,
+            stops: Optional[int] = None,
+            departures: Optional[int] = None
+    ) -> Iterator[Stop]:
         """Yields stops for the given geo coordinates."""
         raise NotImplementedError()
 
-    def get_departures_addr(self, address: Union[Address, str], *,
-                            stops: Optional[int] = None,
-                            departures: Optional[int] = None
-                            ) -> Iterator[Stop]:
+    def get_departures_addr(
+            self,
+            address: Union[Address, str],
+            *,
+            stops: Optional[int] = None,
+            departures: Optional[int] = None
+    ) -> Iterator[Stop]:
         """Yields departures for the given address."""
         raise NotImplementedError()
