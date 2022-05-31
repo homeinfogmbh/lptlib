a15d8acfb7313b96fad2f6380653b71d744d2e54
Richard Neumann <r.neumann@homeinfo.de>
2022-05-31 10:34:04 +0200

    Migrate to setuptools

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='lptlib',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
     maintainer_email='<r dot neumann at homeinfo priod de>',
-    requires=[
+    install_requires=[
         'configlib',
         'functoolsplus',
         'flask',
@@ -20,4 +23,5 @@ setup(
         'wsgilib'
     ],
     packages=['lptlib'],
+    license='GPLv3',
     description='General purpose local public transport API.')
