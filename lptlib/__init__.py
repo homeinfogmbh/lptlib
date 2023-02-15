d21d5c1eccd77ac6132fd947e31bfac2bf5731a0
Richard Neumann <r.neumann@homeinfo.de>
2023-02-15 12:57:07 +0100

    Add WSGI application

--- lptlib/__init__.py
+++ lptlib/__init__.py
@@ -4,9 +4,11 @@ from lptlib.api import get_departures, get_response
 from lptlib.config import get_max_departures, get_max_stops
 from lptlib.datastructures import GeoCoordinates
 from lptlib.exceptions import NoGeoCoordinatesForAddress
+from lptlib.wsgi import APPLICATION
 
 
 __all__ = [
+    'APPLICATION',
     'NoGeoCoordinatesForAddress',
     'get_departures',
     'get_max_departures',
---
+++ lptlib/wsgi.py
@@ -0,0 +1,35 @@
+"""Non-authenticated WSGI interface.
+
+XXX: For internal use only!
+"""
+
+from flask import request
+
+from mdb import Address
+from wsgilib import Application, JSON
+
+from lptlib.api import get_departures
+
+
+__all__ = ['APPLICATION']
+
+
+APPLICATION = Application('lpt', cors=True)
+
+
+@APPLICATION.route('/', methods=['POST'], strict_slashes=False)
+def _get_departures() -> JSON:
+    """Returns the respective departures as a JSON object."""
+
+    return JSON(
+        get_departures(
+            Address(
+                street=request.json['street'],
+                house_number=request.json['houseNumber'],
+                zip_code=request.json['zipCode'],
+                city=request.json['city']
+            ),
+            stops=request.json.get('stops'),
+            departures=request.json.get('departures')
+        ).to_json()
+    )
